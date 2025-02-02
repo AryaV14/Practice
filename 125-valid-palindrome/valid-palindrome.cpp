9a0e85c5f9a7ff940778ec4b1b91baf4abba202f
Arya Vijayan <aryavijayanrohini@gmail.com>
2025-02-02 17:25:46 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (42.06%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -1,40 +1,22 @@
-// class Solution {
-// public:
-//     bool isPalindrome(string s) {
-//         int k=0,j=0,n=s.size();
-//         char temp[n];
-//         for(int i=0;i<s.size();i++)
-//         {
-//             if(97<=s[i] && s[i]<=122 || 48<=s[i] && s[i]<=57 )
-//                 temp[k++]=s[i];
-//             else if(65<=s[i] && s[i]<=90)
-//                 temp[k++]='a'+(s[i]-'A');
-            
-//         }
-//         // cout<<temp;
-//         for(int i=k-1;i>=k/2;i--)
-//         {
-//             if(temp[i]!= temp[j++])
-//                 return false;
-//         }
-//         return true;
-//     }
-// };
-
 class Solution {
 public:
     bool isPalindrome(string s) {
-       int start=0;
-       int end=s.size()-1;
-       while(start<=end){
-           if(!isalnum(s[start])){start++; continue;}
-           if(!isalnum(s[end])){end--;continue;}
-           if(tolower(s[start])!=tolower(s[end]))return false;
-           else{
-               start++;
-               end--;
-           }
-       }
-       return true;
-}
+        int k=0,j=0,n=s.size();
+        char temp[n];
+        for(int i=0;i<s.size();i++)
+        {
+            if(97<=s[i] && s[i]<=122 || 48<=s[i] && s[i]<=57 )
+                temp[k++]=s[i];
+            else if(65<=s[i] && s[i]<=90)
+                temp[k++]='a'+(s[i]-'A');
+            
+        }
+        // cout<<temp;
+        for(int i=k-1;i>=k/2;i--)
+        {
+            if(temp[i]!= temp[j++])
+                return false;
+        }
+        return true;
+    }
 };
